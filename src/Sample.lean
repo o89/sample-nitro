76bd4d114fa73e2ae29b97434e46d5572f8ea1b9
Siegmentation <siegmentationfault@yandex.ru>
2020-04-11 14:55:09 +0700

    https://github.com/leanprover/lean4/commit/746615d81dace4ae961630687e80db3ac67b22f1

--- src/Sample.lean
+++ src/Sample.lean
@@ -5,34 +5,34 @@ inductive Example
 | send
 
 instance : BERT Example :=
-{ toTerm := λ x ⇒ match x with
-  | Example.send ⇒ Term.atom "send",
-  fromTerm := λ x ⇒ match x with
-  | Term.atom "send" ⇒ Sum.ok Example.send
-  | _ ⇒ Sum.fail "invalid Example term" }
+{ toTerm := λ x => match x with
+  | Example.send => Term.atom "send",
+  fromTerm := λ x => match x with
+  | Term.atom "send" => Sum.ok Example.send
+  | _ => Sum.fail "invalid Example term" }
 
 def index : Nitro Example → Result
-| Nitro.init ⇒ update Example "send" $
+| Nitro.init => update Example "send" $
   Elem.button "send" [] "Send"
     { source := [ "msg" ], type := "click", postback := Example.send }
-| Nitro.message Example.send query ⇒
+| Nitro.message Example.send query =>
   match query.lookup "msg" with
-  | some value ⇒ insertBottom Example "hist" (div [] [ Elem.liter value ])
-  | _ ⇒ Result.ok
-| Nitro.error s ⇒ Result.ok
-| Nitro.ping ⇒ pong
-| _ ⇒ Result.ok
+  | some value => insertBottom Example "hist" (div [] [ Elem.liter value ])
+  | _ => Result.ok
+| Nitro.error s => Result.ok
+| Nitro.ping => pong
+| _ => Result.ok
 
 def about : Nitro Example → Result
-| Nitro.init ⇒ updateText "app" "This is the N2O Hello World App"
-| Nitro.ping ⇒ pong
-| _ ⇒ Result.ok
+| Nitro.init => updateText "app" "This is the N2O Hello World App"
+| Nitro.ping => pong
+| _ => Result.ok
 
 def router (cx : Nitro.cx Example) : Nitro.cx Example :=
 let handler := match Req.path cx.req with
-| "/ws/static/index.html" ⇒ index
-| "/ws/static/about.html" ⇒ about
-| _ ⇒ ignore;
+| "/ws/static/index.html" => index
+| "/ws/static/about.html" => about
+| _ => ignore;
 ⟨cx.req, handler⟩
 
 def handler : Handler := mkHandler (nitroProto Example) [ router ]
